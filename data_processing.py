0542dab0cc579279a573e3bc8567768daa3fdbe1
anumeha1986 <anumeha.sinha06@gmail.com>
2023-03-13 14:10:40 +0530

    correct sum and added multipication

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,15 @@
 print("adding all the code for fraud detection ")
+
+
+a = 5
+b=3
+
+def sum(a,b):
+    c = a+b
+    return c
+print("sum function")
+
+def multiplication(a,b):
+    d = a*b
+    return d
+print("Multiplication function")
