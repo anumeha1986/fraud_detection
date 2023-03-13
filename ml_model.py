caf4467883152f84672a8ea2e6d8ca5fa9f96429
anumeha1986 <anumeha.sinha06@gmail.com>
2023-03-13 14:26:34 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,13 @@
 print("adding all the mdeol code here")
+
+
+def model1():
+    print("adding code for ML model1")
+
+    
+def model2():
+    print("adding code for ML model2")
+
+    
+def model3():
+    print("adding code for ML model3")
